6336ad96282a5f5b33132044127c1c20be1aa2ae
ST10159832 <129107972+ST10159832@users.noreply.github.com>
2024-05-30 21:16:33 +0200

    Add files via upload
    this commit is being done after the internal classes have been added

---
+++ ST10159832_Part2PoeProg6221/ST10159832_Part2PoeProg6221/Ingredients.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ST10159832_Part2PoeProg6221
+{
+    internal class Ingredients
+    {
+    }
+}
